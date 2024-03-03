110c1bfd254061807a16dafc468f2c8cf5680806
Ross <83570379+thanhtayt@users.noreply.github.com>
2024-03-03 14:58:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 async function generateRandomImage() {
-    const apiKey = '42672181-94ab2764da5b745c3a762f97d';
-    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=nature&image_type=photo`;
+    const width = 500; // Kích thước ảnh (chiều rộng)
+    const height = 300; // Kích thước ảnh (chiều cao)
+    const apiUrl = `https://picsum.photos/${width}/${height}`;
 
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-
         const randomImage = document.getElementById("randomImage");
-        const randomIndex = Math.floor(Math.random() * data.hits.length);
-        randomImage.src = `${data.hits[randomIndex].largeImageURL}?random=${Math.random()}`;
+        randomImage.src = response.url;
     } catch (error) {
         console.error('Error fetching image:', error);
     }
