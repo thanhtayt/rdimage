245e21018c76661b0cae60122e06e560f3344d1d
Ross <83570379+thanhtayt@users.noreply.github.com>
2024-03-03 14:18:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 async function generateRandomImage() {
-    const accessKey = 'HkWEghBdGpqOCTyXQ_F_mWO32ayCGO0HglfKgq89Whk';
-    const apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
+    const apiKey = 'PtSfavOuLJSBm9TR7DHCV1fKueYGfyt44dASK4t8ErOQEitURMxnoJs4';
+    const apiUrl = `https://api.pexels.com/v1/curated?per_page=1`;
 
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl, {
+            headers: {
+                Authorization: apiKey
+            }
+        });
         const data = await response.json();
 
         const randomImage = document.getElementById("randomImage");
-        randomImage.src = data.urls.regular;
+        randomImage.src = data.photos[0].src.large;
     } catch (error) {
         console.error('Error fetching image:', error);
     }
