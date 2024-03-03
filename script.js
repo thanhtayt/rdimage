887a43b92be7d6f237a7c81e8a78265bb5b0c56a
Ross <83570379+thanhtayt@users.noreply.github.com>
2024-03-03 15:21:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 async function generateRandomAnimeImage() {
     const apiUrl = 'https://api.waifu.pics/sfw/waifu';
-
+    
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
         const randomAnimeImage = document.getElementById("randomAnimeImage");
         randomAnimeImage.src = data.url;
+
+        const downloadLink = document.getElementById("downloadLink");
+        downloadLink.href = data.url;
+        downloadLink.style.display = 'inline'; // Hiển thị nút tải xuống
     } catch (error) {
         console.error('Error fetching image:', error);
     }
